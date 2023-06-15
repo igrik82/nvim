393994f9183b51a854d3a2739e948d527c19b21c
Igor <igrik82@gmail.com>
2023-06-15 19:35:31 +0500

    Moved mapped debug key to wihich-key plugin

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,19 +19,20 @@ keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
 --Debuger
-keymap.set("n", "<C-d>", ":lua require('dapui').toggle()<CR>") -- Start debugging
-keymap.set("n", "<C-b>", ":lua require'dap'.toggle_breakpoint()<CR>") -- Setting breakpoints
-keymap.set("n", "<leader>b", ":lua require'dap'.set_breakpoint(vim.fn.input('Condition: '), nil, nil)<CR>") -- Setting conditional breakpoints
-keymap.set("n", "<C-c>", ":lua require'dap'.continue()<CR>") -- Launching debug sessions
-keymap.set("n", "<C-o>", ":lua require'dap'.step_over()<CR>") -- Step over
-keymap.set("n", "<C-i>", ":lua require'dap'.step_into()<CR>") -- Step into
-keymap.set("n", "<leader>o", ":lua require'dap'.repl.open()<CR>") -- Inspecting the state
+--keymap.set("n", "<C-d>", ":lua require('dapui').toggle()<CR>") -- Start debugging
+--keymap.set("n", "<C-b>", ":lua require'dap'.toggle_breakpoint()<CR>") -- Setting breakpoints
+--keymap.set("n", "<leader>b", ":lua require'dap'.set_breakpoint(vim.fn.input('Condition: '), nil, nil)<CR>") -- Setting conditional breakpoints
+--keymap.set("n", "<C-c>", ":lua require'dap'.continue()<CR>") -- Launching debug sessions
+--keymap.set("n", "<C-o>", ":lua require'dap'.step_over()<CR>") -- Step over
+--keymap.set("n", "<C-i>", ":lua require'dap'.step_into()<CR>") -- Step into
+--keymap.set("n", "<leader>o", ":lua require'dap'.repl.open()<CR>") -- Inspecting the state
 
 -- Execute current python file
-keymap.set("n", "<leader>r", ":w<CR>:belowright split |terminal python %<CR>")
+--keymap.set("n", "<leader>r", ":w<CR>:belowright split |terminal python %<CR>")
+--keymap.set("n", "<leader>r", ":w<CR>:FloatermNew --height=0.9 --width=0.9 --autoclose=0 python %<CR>")
 
 -- Copy micropython file to esp32
-keymap.set("n", "<leader>cv", ":w<CR>:! rshell --buffer-size=30 -p /dev/ttyUSB0 cp % /pyboard<CR>")
+--keymap.set("n", "<leader>cv", ":w<CR>:! rshell --buffer-size=30 -p /dev/ttyUSB0 cp % /pyboard<CR>")
 
 -- Execute micropython current file in esp32
-keymap.set("n", "<leader>e", ":w<CR>:! rshell --buffer-size=30 -p /dev/ttyUSB0 repl pyboard import %:r<CR>")
+--keymap.set("n", "<leader>e", ":w<CR>:! rshell --buffer-size=30 -p /dev/ttyUSB0 repl pyboard import %:r<CR>")
