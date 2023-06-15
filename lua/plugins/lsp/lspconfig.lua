6b93d713c6739367529f136f5b56307811d57f88
Igor <igrik82@gmail.com>
2023-06-15 19:38:43 +0500

    Added key map for LSP Saga

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,7 +1,6 @@
 -- import lspconfig plugin safely
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
 end
 
 -- import cmp-nvim-lsp plugin safely
