63ebc990d1acd3f3d1471b8c86f906ab788bd814
Igor <igrik82@gmail.com>
2023-06-15 19:41:53 +0500

    added key binding for debug to plugin

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,26 @@
 vim.o.timeout = true
-vim.o.timeoutlen = 300
-require("which-key").setup({
-	-- your configuration comes here
-	-- or leave it empty to use the default settings
-	-- refer to the configuration section below
-})
+vim.o.timeoutlen = 500
+local wk = require("which-key")
+local mappings = {
+	-- Python
+	p = { ":w<CR> :FloatermNew --height=0.9 --width=0.9 --autoclose=0 python %<CR>", "Run python script" },
+	--LazyGit
+	l = { ":w<CR> :FloatermNew --height=0.9 --width=0.9 --autoclose=2 lazygit<CR>", "Launch LazyGit" },
+
+	--Debuger
+	d = "Debug",
+	dd = { ":lua require('dapui').toggle()<CR>", "Start debugging" },
+	db = { ":lua require'dap'.toggle_breakpoint()<CR>", "Set brealpoint" },
+	dB = { ":lua require'dap'.set_breakpoint(vim.fn.input('Condition: '), nil, nil)<CR>", "Conditional breakpoint" },
+	dc = { ":lua require'dap'.continue()<CR>", "Debug continue" },
+	dO = { ":lua require'dap'.step_over()<CR>", "Debug step over" },
+	di = { ":lua require'dap'.step_into()<CR>", "Debug step into" },
+	ds = { ":lua require'dap'.repl.open()<CR>", "Debug inspect state" },
+
+	--Telescope
+	f = "Telescope",
+}
+
+local opts = { prefix = "<leader>" }
+
+wk.register(mappings, opts)
