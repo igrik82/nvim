24c3a32d9485edc016ca802732039be106c2db43
Igor <igrik82@gmail.com>
2024-03-15 14:15:39 +0500

    fix: syntax

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -23,7 +23,8 @@ keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
 -- Execute current python file in terminal
-keymap.set("n", "<leader>t", ":w<CR>:belowright split |terminal python %<CR>")
+-- keymap.set("n", "<leader>t", ":w<CR>:belowright split |terminal python %<CR>")
+keymap.set("n", "<leader>t", ":w<CR>:belowright split |terminal ./compile.sh %<CR>")
 --keymap.set("n", "<leader>r", ":w<CR>:FloatermNew --height=0.9 --width=0.9 --autoclose=0 python %<CR>")
 
 -- Copy micropython file to esp32
